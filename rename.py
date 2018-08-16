e305708846085b86b7650cdfb17c64aa4a3b77c3
Viktor <viktorpar@gmail.com>
2018-08-16 08:58:58 +0300

    added select folder

---
+++ rename.py
@@ -0,0 +1,41 @@
+import os
+import re
+# from tkinter.filedialog import askopenfilename
+import tkinter.filedialog as tk
+path = tk.askdirectory(
+    initialdir='C:/Users/spri85/Videos/4K Video Downloader',
+    title='Select folder to lessons'
+)
+# path = os.getcwd()
+
+filenames = os.listdir(path)
+
+pattern = re.compile(r"(?P<lesson_number>[\d]+)(?P<extention>\.mp4)", re.I)
+
+
+def count_lessons(filenames, pattern):
+    total_lessons = 0
+    for lesson in filenames:
+        if pattern.search(lesson):
+            total_lessons += 1
+    return total_lessons
+
+
+def rename(filenames, pattern):
+    total_lessons = count_lessons(filenames, pattern)
+
+    for filename in filenames:
+        result = pattern.search(filename)
+
+        if result:
+            prefix_count = '0'*(len(str(total_lessons)) -
+                                len(result.group('lesson_number')))
+            new_name = (prefix_count+result.group('lesson_number') +
+                        result.group('extention'))
+            try:
+                os.rename(filename, new_name)
+            except FileExistsError:
+                print(f'You already have a file with name: {new_name}')
+
+
+rename(filenames, pattern)
